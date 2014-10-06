7641ed246a8b2768525046386e8b5c0a0f208737
Te Vallee <astronomemusic@gmail.com>
2014-10-05 20:26:23 -1000

    commented var currentFloor and var destination out so the first 7 tests would pass and the last 4 would fail.

--- elevator.js
+++ elevator.js
@@ -6,16 +6,16 @@ module.exports = {
   goto: function(currentFloor,destination){  
     //floor = result
     var floor = 0;
-    var destination =0;
-    var currentFloor = 0;
+    //var currentFloor =0;
+    //var destination =0;
 
-    //if any floor above the 4th or below the ground floor is chosen, the result should return 0
+    //if any floor above the 4th is chosen, the result will return 0
     if(destination >3){
       floor = 0;
 
       return floor;
     }
-
+    //if any floor below the ground floor is chosen,the result will return 0
     if(currentFloor >3 && destination <1){
       floor =0;
 
