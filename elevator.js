3388418f85575e393e93157e6f68ad12f94c1606
Te Vallee <astronomemusic@gmail.com>
2014-10-05 22:25:45 -1000

    set first param type to number and second param type to string

--- elevator.js
+++ elevator.js
@@ -1,32 +1,34 @@
 /*jslint node: true */
 'use strict';
 
+
+
 module.exports = {
 
-  goto: function(currentFloor,destination){  
+  goto: function(currentFloor,destination,floor){  
     //floor = result
     var floor = 0;
-    //var currentFloor =0;
-    //var destination =0;
-
-    //if any floor above the 4th is chosen, the result will return 0
-    if(parseInt(destination) >3){
-      floor = 0;
-
+    //set first param currentFloor to number type and second param destination to string type
+    if(typeof currentFloor ==='number' && typeof destination ==='string'){
+      //if any floor above the 4th is chosen, the result will return 0
+      if(destination >3){
+        floor = 0;
+  
+        return floor;
+      }
+      //if any floor below the ground floor is chosen,the result will return 0
+      if(currentFloor >3 && destination <1){
+        floor =0;
+  
+        return floor;
+      }
+  
+      //desired floor subtracted by the current floor will give the result
+      floor = destination - currentFloor;
+  
       return floor;
+  
     }
-    //if any floor below the ground floor is chosen,the result will return 0
-    if(currentFloor >3 && parseInt(destination) <1){
-      floor =0;
-
-      return floor;
-    }
-
-    //desired floor subtracted by the current floor will give the result
-    floor = parseInt(destination) - currentFloor;
-
-    return floor;
-
   }
 
 };
