668c76d201ee24f641736cb07b96222f4ea75b01
Te Vallee <astronomemusic@gmail.com>
2014-10-05 18:44:51 -1000

    set var floor to equal to destination subtracted by currentFloor. 5 tests pass.

--- elevator.js
+++ elevator.js
@@ -1,40 +1,15 @@
 /*jslint node: true */
 'use strict';
 
-
 module.exports = {
-  goto: function(currentFloor,destination){
+
+  goto: function(currentFloor,destination){  
+
+    var floor = 0;
+
+    floor = destination - currentFloor;
+
+    return floor;
+  }
   
-  var currentFloor = 0;
-  var buttonPushed = prompt("floor 0, 1, 2, or 3?");
-
-  //if pasenger pushes 0
-  if(buttonPushed = "0"){
-    if(currentFloor=0){
-      return "Ground Floor";
-    }
-
-    else if(currentFloor>0){
-      math.abs(destination)= buttonPushed - currentFloor;
-      return destination;
-    }
-  }
-
-  else if(buttonPushed = "1"){
-    if(currentFloor=1){
-      return "1";
-    }
-
-    else if(currentFloor<1){
-      destination = currentFloor + buttonPushed;
-      return destination;
-    }
-
-    else if(currentFloor>)
-  }
-
-  return floor;
-  }
 };
-
-
