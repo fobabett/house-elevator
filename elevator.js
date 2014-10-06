a2bd38c0a74ab89c28b4e6ef5045c84f4355736e
Te Vallee <astronomemusic@gmail.com>
2014-10-05 19:03:17 -1000

    6 tests pass

--- elevator.js
+++ elevator.js
@@ -6,16 +6,19 @@ module.exports = {
   goto: function(currentFloor,destination){  
     //floor = result
     var floor = 0;
-    //desired floor subtracted by the current floor will give the result
-    floor = destination - currentFloor;
 
-    return floor;
     //if any floor above the 4th or below the ground floor is chosen, the result should return 0
     if(destination >3 || destination <0){
       floor = 0;
 
       return floor;
     }
+
+    //desired floor subtracted by the current floor will give the result
+    floor = destination - currentFloor;
+
+    return floor;
+
   }
 
 };
